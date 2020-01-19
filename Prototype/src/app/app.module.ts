81f99ef7a2e3d2d1074acf112c3eb79370696945
ct401 <45435701+ct401@users.noreply.github.com>
2020-01-19 17:26:49 +0000

    Update app.module.ts

--- Prototype/src/app/app.module.ts
+++ Prototype/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { NgxTwitterTimelineModule } from 'ngx-twitter-timeline';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxTwitterTimelineModule
+    NgxTwitterTimelineModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
