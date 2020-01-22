691287bd0931a69b6b05697971ad0200f0a476fe
dkingston97 <56847194+dkingston97@users.noreply.github.com>
2020-01-22 20:50:53 +0000

    Added Reddit component

--- Prototype/src/app/social-media/social-media.component.ts
+++ Prototype/src/app/social-media/social-media.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-social-media',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./social-media.component.sass']
 })
 export class SocialMediaComponent implements OnInit {
+  @Output() socialMedia = new EventEmitter<boolean>();
+  
+  public socialMediaForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    this.socialMediaForm = this.formBuilder.group({
+      currency: ['', Validators.required],
+      StartDate: ['', Validators.required],
+      EndDate: ['', Validators.required]
+    });
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
+  get f() { return this.socialMediaForm.controls; }
 
+  public onSubmit() {
+    console.log(this.f.currency.value + this.f.StartDate.value + this.f.EndDate.value);
+    this.socialMedia.emit(true);
+    return false;
+    // if (this.loginForm.valid) {
+      
+    // }
+  }
 }
+
