b3f2c02e299a863354d51aa62a44c851a3908921
ct401 <45435701+ct401@users.noreply.github.com>
2020-02-12 00:22:13 +0000

    Add files via upload

---
+++ CryptoCompareApiData/C# Implementation/Model/BitcoinEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace CryptoCompareBitcoinData.Model
+{
+    class BitcoinEntity : TableEntity
+    {
+        public BitcoinEntity()
+        {
+        }
+
+        public BitcoinEntity(string time, string open, string close)
+        {
+            PartitionKey = time;
+            RowKey = open;
+        }
+
+        public string SomeData { get; set; }
+        public string SomeMoreData { get; set; }
+    }
+}
