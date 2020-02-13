271fa34b8704504d054ad33490a8ae873936dae8
mem277 <60063420+mem277@users.noreply.github.com>
2020-02-13 16:45:53 +0000

    updated positions component

--- Prototype/src/app/positions/positions.component.ts
+++ Prototype/src/app/positions/positions.component.ts
@@ -1,15 +1,80 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 
-@Component({
+export class PositionsRequest {
+  timestamp: Date
+  userName: string
+  currency: string
+  quantity: BigInteger
+  positionType: string
+}
+
+@Component ({
   selector: 'app-positions',
   templateUrl: './positions.component.html',
   styleUrls: ['./positions.component.sass']
 })
+
 export class PositionsComponent implements OnInit {
 
-  constructor() { }
+  public positionsForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-  ngOnInit() {
+  httpOptions = {
+    headers: new HttpHeaders ({
+      'Content-Type':  'application/json',
+    })
+  };
+  
+  constructor(private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router, 
+    private http: HttpClient) { }
+    
+    ngOnInit() { 
+      this.positionsForm = this.formBuilder.group ({
+      buyQuantity: ['', Validators.required],
+      sellQuantity: ['', Validators.required]
+    });
+    
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
+  
+  get f() { return this.positionsForm.controls; }
 
+  public async onBuy() {
+    
+    let req = new PositionsRequest();
+    //req.timestamp = this.f.timestamp.value
+    req.userName = this.f.username.value
+    req.currency = "BITCOIN"
+    req.quantity = this.f.positionQuantity.value
+    req.positionType = this.f.positionType.value
+    
+    await this.http.post<any>('https://uokgpwebapi.azurewebsites.net/api/positions/insert', req, this.httpOptions).subscribe(response => {
+      if (response["statusCode"] == 200) {
+        console.log(this.f.username.value, this.f.positionQuantity.value, this.f.positionType.value)
+      }
+    });
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
